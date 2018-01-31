e1991afb2c963e39725cd3479397c45245f8d573
tongyiwangzaimilk <sdplwyt@163.com>
2018-01-31 19:32:50 +0800

    one tree

--- Blending/topologyTree.js
+++ Blending/topologyTree.js
@@ -3,6 +3,7 @@
  */
 var ptree1=[],ptree2=[],blendtree=[];//增添零枝干后的树1树2
 var topotree1,topotree2;//树1 和树2 的拓扑结构
+var branch;
 function Node(data) {
     this.data = data;
     this.leftNode = null;
@@ -19,7 +20,8 @@ function topologyTree(tree1,tree2){
     //firstLayer();
     //nextLayer();
     blending();
-    createTree();
+    compact();
+    drawTree();
 }
 //后层拓扑结构处理
 function nextLayer(){
@@ -134,28 +136,124 @@ function blendBranch(trunk1,trunk2){
             position = new THREE.Vector3(trunk1[i].pos.x, trunk1[i].pos.y, trunk1[i].pos.z);
             circle = {
                 radius: (trunk1[i].radius) / 2,
-                pos: position.divideScalar(2)
+                pos: position.divideScalar(2),
+                child:trunk1[i].child,
+                position:trunk1[i].position
             };
         }
         else if (trunk1 == "0") {
             position = new THREE.Vector3(trunk2[i].pos.x, trunk2[i].pos.y, trunk2[i].pos.z);
             circle = {
                 radius: (trunk2[i].radius) / 2,
-                pos: position.divideScalar(2)
+                pos: position.divideScalar(2),
+                child:trunk2[i].child,
+                position:trunk2[i].position
             }
         }
         else if (i < trunk1.length && i < trunk2.length) {
             position = new THREE.Vector3(trunk1[i].pos.x + trunk2[i].pos.x, trunk1[i].pos.y + trunk2[i].pos.y, trunk1[i].pos.z + trunk2[i].pos.z);
-            circle = {
-                radius: (trunk1[i].radius + trunk2[i].radius) / 2,
-                pos: position.divideScalar(2)
-            };
+            if(trunk1.length<trunk2.length) {
+                circle = {
+                    radius: (trunk1[i].radius + trunk2[i].radius) / 2,
+                    pos: position.divideScalar(2),
+                    child:trunk2[i].child,
+                    position:trunk2[i].position
+                };
+            }
+            else{
+                circle = {
+                    radius: (trunk1[i].radius + trunk2[i].radius) / 2,
+                    pos: position.divideScalar(2),
+                    child:trunk1[i].child,
+                    position:trunk1[i].position
+                };
+            }
         }
         trunk.push(circle);
     }
     return trunk;
 }
 //层次结构转换为树
-function createTree(){
+function drawTree(){
+    for(var i=0;i<blendtree.length;i++) {
+        for(var j=0;j<blendtree[i].length;j++) {
+            drawBranch(blendtree[i][j]);
+            scene.add(branch);
+        }
+    }
+}
+function drawBranch(trunk) {
+    var seg = 30;
+    var geo = new THREE.Geometry();
+    for(var i = 0, l = trunk.length; i < l; i ++){
+        var circle = trunk[i];
+        for(var s=0;s<seg;s++){//for each point in the circle
+            var rd = circle.radius;
+            var pos = new THREE.Vector3(0,0,0);
+            var posx=0,posy=0,posz=0;
+            if(i>0) {
+                posx = Math.abs(trunk[i].pos.x - trunk[i - 1].pos.x);
+                posy = Math.abs(trunk[i].pos.y - trunk[i - 1].pos.y);
+                posz = Math.abs(trunk[i].pos.z - trunk[i - 1].pos.z);
+            }
+            if(i==0){
+                posx = Math.abs(trunk[i+1].pos.x - trunk[i].pos.x);
+                posy = Math.abs(trunk[i+1].pos.y - trunk[i].pos.y);
+                posz = Math.abs(trunk[i+1].pos.z - trunk[i].pos.z);
+            }
+            if(posx>=posy&&posx>=posz) {
+                pos.x = 0;
+                pos.y = rd * Math.sin(2 * Math.PI / seg * s);
+                pos.z = rd * Math.cos(2 * Math.PI / seg * s);
+            }
+            if(posz>=posx&&posz>=posy){
+                pos.x = rd * Math.sin(2 * Math.PI / seg * s);
+                pos.y = rd * Math.cos(2 * Math.PI / seg * s);
+                pos.z = 0;
+            }
+            if(posy>=posz&&posy>=posx) {
+                pos.x = rd * Math.cos(2 * Math.PI / seg * s);
+                pos.y = 0;
+                pos.z = rd * Math.sin(2 * Math.PI / seg * s);
+            }
+            geo.vertices.push(pos.add(circle.pos));
+        }
+    }
 
+    for(i=0;i<l-1;i++){
+        for(s=0;s<seg;s++){
+            var v1 = i*seg+s;
+            var v2 = i*seg+(s+1)%seg;
+            var v3 = (i+1)*seg+(s+1)%seg;
+            var v4 = (i+1)*seg+s;
+
+            geo.faces.push(new THREE.Face3(v1,v2,v3));
+            geo.faceVertexUvs[0].push([new THREE.Vector2(s/seg,0),new THREE.Vector2((s+1)/seg,0),new THREE.Vector2((s+1)/seg,1)]);
+            geo.faces.push(new THREE.Face3(v3,v4,v1));
+            geo.faceVertexUvs[0].push([new THREE.Vector2((s+1)/seg,1),new THREE.Vector2((s)/seg,1),new THREE.Vector2((s)/seg,0)]);
+        }
+    }//add faces and uv
+    geo.computeFaceNormals();
+    branch = new THREE.Mesh(geo,new THREE.MeshLambertMaterial({
+        // wireframe:true,
+        side:THREE.DoubleSide,
+        map:THREE.ImageUtils.loadTexture("../textures/tree/timg.jpg")
+    }));
+}
+//紧凑化处理
+function compact(){
+    for(var i=1;i<blendtree.length;i++){
+        for(var j=0;j<blendtree[i].length;j++){
+            var child = blendtree[i][j][0].child;
+            var position = blendtree[i][j][0].position;
+            var x = blendtree[i-1][child][position].pos.x - blendtree[i][j][0].pos.x;
+            var y = blendtree[i-1][child][position].pos.y - blendtree[i][j][0].pos.y;
+            var z = blendtree[i-1][child][position].pos.z - blendtree[i][j][0].pos.z;
+            for(var m=0;m<blendtree[i][j].length;m++){
+                blendtree[i][j][m].pos.x += x;
+                blendtree[i][j][m].pos.y += y;
+                blendtree[i][j][m].pos.z += z;
+            }
+        }
+    }
 }
