34fb69944e0ff66dcc3fc9949274a544904f346f
tongyiwangzaimilk <sdplwyt@163.com>
2018-02-10 16:35:06 +0800

    how to reuse?

--- Blending/reuse.js
+++ Blending/reuse.js
@@ -2,7 +2,8 @@
  * Created by deii66 on 2018/2/8.
  */
 var reusableset = [];
-function reusableSet(trunk){
+//把枝干移动到坐标轴原点
+function movetoOrigin(trunk){
     var x = trunk[0].pos.x - 0;
     var y = trunk[0].pos.y - 0;
     var z = trunk[0].pos.z - 0;
@@ -15,5 +16,14 @@ function reusableSet(trunk){
         rtrunk[i].pos.y -= y;
         rtrunk[i].pos.z -= z;
     }
-    reusableset.push(rtrunk);
+    return rtrunk;
+}
+//reusabset为原始树木的枝干移动到零点后的集合
+function reusableSet(trunk){
+    reusableset.push(movetoOrigin(trunk));
+}
+//将过渡枝干与reusableset中的枝干进行对比
+function compare(trunk){
+    var ctrunk = movetoOrigin(trunk);
+    
 }
